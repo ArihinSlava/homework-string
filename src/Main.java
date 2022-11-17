704d5264a3a7dccd067f499ea43dc0e1961ae3dc
Arihin <arihin.slava@yandex.ru>
2022-11-17 17:54:03 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
         String middleName = " Ivanovich";
         String lastName = " Ivanov";
         String fullName = lastName + firstName + middleName;
-        System.out.println(fullName);
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        /// Задача 2
+        System.out.println(" Задача 2 ");
+        String FullName1 = fullName.toUpperCase();
+        System.out.println("Данный ФИО сотрудника для заполнения отчета - " + FullName1);
     }
 }
 
